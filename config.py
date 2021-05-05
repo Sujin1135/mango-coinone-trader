c1ba1980051618789b646fea0ccf4907e6f371d5
mango <mchead1@naver.com>
2021-05-05 16:57:23 +0900

    init commit

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+# Coinone API configurations
+API_NAME = os.environ['COINONE_API_NAME']
+ACCESS_TOKEN = os.environ['COINONE_ACCESS_TOKEN']
+SECRET_KEY = os.environ['COINONE_SECRET_KEY']
