f0591ddf14f722d5f33a14f72d42f35ede2579bc
mango <mchead1@naver.com>
2021-07-17 17:20:30 +0900

    add: the request test case to coinone api

---
+++ tests/src/providers/coinone_req_test_case.py
@@ -0,0 +1,16 @@
+import unittest
+from config import ACCESS_TOKEN
+from src.providers.coinone_req import CoinoneReq
+
+
+class CoinoneReqTestCase(unittest.TestCase):
+    def setUp(self):
+        self.req = CoinoneReq()
+
+    def test_get_balance(self):
+        payload = {"access_token": ACCESS_TOKEN}
+        req = CoinoneReq()
+
+        sut = req.get_post_response(action='v2/account/balance', payload=payload)
+
+        self.assertEqual(sut['result'], 'success')
