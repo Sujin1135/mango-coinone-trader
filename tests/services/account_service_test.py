722deb36de447ac398946d0397a8e67324274f80
chris <mchead1@naver.com>
2021-09-20 17:58:58 +0900

    :page_facing_up: 매수 취소기능 추가 / 테스트 케이스 작성

--- tests/services/account_service_test.py
+++ tests/services/account_service_test.py
@@ -56,18 +56,30 @@ def test_get_my_deposit_address():
 
 
 def test_sell_eth_little():
-    sut = sell(4071000, 0.015, 'ETH')
+    sut = sell(4051000, 0.0015, 'ETH')
 
     assert sut['result'] == "success"
 
 
 # TODO: 바로 매도가 되지 않을만한 가격을 얻어서 매도 요청을 하도록 수정
 def test_cancel_sell():
-    price = 4071000
+    price = 3700000
     qty = 0.0015
     currency = 'ETH'
     result = sell(price, qty, currency)
     order_id = result['orderId']
 
     sut = cancel_sell(order_id, price, qty, currency)
+    assert sut["result"] == "success"
+
+
+# TODO: 바로 매수가 되지 않을만한 가격을 얻어서 매수 요청을 하도록 수정
+def test_cancel_buy():
+    price = 2000000
+    qty = 0.005
+    currency = 'ETH'
+    result = buy(price, qty, currency)
+    order_id = result['orderId']
+
+    sut = cancel_buy(order_id, price, qty, currency)
     assert sut["result"] == "success"
