1254ca8fec55b8a2ab0c008a873e87f1fb956b9f
chris <mchead1@naver.com>
2021-09-26 02:39:44 +0900

    :necktie: main 메서드에서 IDE에서 바로 실행가능하도록 로직 추가

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import uvicorn
 from app.core.config import settings
-from app.apis.v1.api import router
+from app.apis.v1.api import api_router
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
-app.include_router(router, prefix=settings.API_V1)
+app.include_router(api_router, prefix=settings.API_V1)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        log_level="info",
+    )
